4185542c0d88a6366bd509d18b39896bf3aed9b4
DannyLien <danny.lien1109@gmail.com>
2022-05-29 16:28:00 +0800

    711 gui Button ActionListener class, 匿名, Lamda

--- src/main/java/com/tom/gui/MainFram.java
+++ src/main/java/com/tom/gui/MainFram.java
@@ -1,19 +1,40 @@
 package com.tom.gui;
 
+import org.w3c.dom.ls.LSOutput;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainFram extends JFrame {
     JPanel panel = new JPanel();
-    JButton ok = new JButton("OKlalalalalala");
+    JButton ok = new JButton("OK");
     JButton cancel = new JButton("Cancel");
 
     public MainFram() {
-        setSize(600,400);
-        setLocation(200,200);
+        setSize(600, 400);
+        setLocation(200, 200);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         panel.add(ok);
         panel.add(cancel);
         add(panel);
+//        ActionListener listener = new MyListener();
+//        ok.addActionListener(listener);
+//        ActionListener byeListener = new ByeListener();
+//        cancel.addActionListener(byeListener);
+
+        ok.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Hello main");
+            }
+        });
+
+        cancel.addActionListener((actionEvent) -> {
+            System.out.println("Bye main Lamda");
+        });
+
         setVisible(true);
     }
 
---
+++ src/main/java/com/tom/gui/MyListener.java
@@ -0,0 +1,11 @@
+package com.tom.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Hello??");
+    }
+}
